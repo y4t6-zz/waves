4848ae37e10f5aef5fdcc89cd64672ced4f2f750
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-12 11:14:05 +0300

    L87. CLIENT Finishing the login

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -1,4 +1,20 @@
-import axiox from 'axios';
+import axios from 'axios';
+import {
+    LOGIN_USER
+} from "./types";
 
 import { USER_SERVER } from '../components/utils/misc';
 
+
+
+export function loginUser(dataToSubmit) {
+
+    const request = axios.post(`${USER_SERVER}/login`, dataToSubmit)
+        .then(response => response.data);
+
+    return {
+        type: LOGIN_USER,
+        payload: request
+    }
+
+}
--- client/src/components/Register_login/login.js
+++ client/src/components/Register_login/login.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import FormField from '../utils/Form/formfield';
 import { update, generateData, isFormValid } from '../utils/Form/formActions';
+import { withRouter } from 'react-router-dom';
 
 import { connect } from 'react-redux';
+import { loginUser } from '../../actions/user_actions';
 
 class Login extends Component {
 
@@ -59,7 +61,16 @@ class Login extends Component {
         let formIsValid = isFormValid(this.state.formdata, 'login');
 
         if(formIsValid) {
-            console.log(dataToSubmit)
+            this.props.dispatch(loginUser(dataToSubmit)).then(response => {
+               if(response.payload.loginSuccess) {
+                    this.props.history.push('/user/dashboard');
+               } else {
+                   this.setState({
+                       formError: true
+                   })
+               }
+            });
+
         } else {
             this.setState({
                 formError: true
@@ -99,4 +110,4 @@ class Login extends Component {
     }
 }
 
-export default connect()(Login);
+export default connect()(withRouter(Login));
