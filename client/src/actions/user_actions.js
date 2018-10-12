65338aa90ec3920651e078fc3a3a5128b81e45cf
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-12 19:20:49 +0300

    L89. CLIENT Finishing register

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from "./types";
 
 import { USER_SERVER } from '../components/utils/misc';
 
+export function registerUser(dataToSubmit) {
 
+    const req = axios.post(`${USER_SERVER}/register`, dataToSubmit)
+        .then(response => response.data);
+
+    return {
+        type: REGISTER_USER,
+        payload: req
+    }
+
+}
 
 export function loginUser(dataToSubmit) {
 
--- client/src/components/utils/Form/formActions.js
+++ client/src/components/utils/Form/formActions.js
@@ -8,6 +8,12 @@ export const validate = (element, formdata=[]) => {
         error = !valid ? [valid, message] : error;
     }
 
+    if(element.validation.confirm) {
+        const valid = element.value.trim() === formdata[element.validation.confirm].value;
+        const message = `${!valid ? 'Password do not patch!' : ''}`;
+        error = !valid ? [valid, message] : error;
+    }
+
     if(element.validation.required) {
         const valid = element.value.trim() !== '';
         const message = `${!valid ? 'This field is required!' : ''}`;
@@ -45,7 +51,9 @@ export const generateData = (formdata, formName) => {
     let dataToSubmit = {};
 
     for(let key in formdata) {
-        dataToSubmit[key] = formdata[key].value;
+        if(key !== 'confirmPassword') {
+            dataToSubmit[key] = formdata[key].value;
+        }
     }
 
     return dataToSubmit;
